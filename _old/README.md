1d2f21b49da35d00cd1a8de09effaf510b777007
Josh Brunner <joshuaebrunner@gmail.com>
2017-01-29 21:37:43 -0800

    Initial commit of refactor

---
+++ _old/README.md
@@ -0,0 +1,11 @@
+# Set Up Preview
+1. `cd` into the `src` folder
+2. type `wintersmith preview`
+3. Code.
+
+# Build Project
+1. `cd` into the `src` folder
+2. type `wintersmith build`
+3. push code to `origin master`
+
+*Built using Wintersmith*
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>what about this?</h2>
+        </div>
+        <p className="App-intro">
+          To get stfdfdfdarted, edit <code>srcffff/App.js</code> and save to reload.
+        </p>
+        <p>hello world!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
