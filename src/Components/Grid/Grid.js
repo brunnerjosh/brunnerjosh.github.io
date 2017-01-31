624d2bcf8fc28010f4092d6b2b831989ac5e06e5
Josh Brunner <josh@helpfulhuman.com>
2017-01-31 13:02:07 -0800

    Added ability to set cell background from props

--- src/Components/Grid/Grid.js
+++ src/Components/Grid/Grid.js
@@ -3,9 +3,16 @@ import GridCell from './GridCell';
 import './Grid.css';
 
 export default class Grid extends React.Component {
+
+  determineCellBackground (index) {
+    console.log('index', index);
+    return 'red';
+  }
+
   renderGridCells () {
-    return this.props.items.map (item => {
-      return <GridCell item={item} />
+    return this.props.items.map( (item, index) => {
+      const cellBg = this.determineCellBackground(index);
+      return <GridCell key={index} item={item} background={cellBg} />
     })
   }
 
