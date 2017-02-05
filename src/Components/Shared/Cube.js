534a8d32762f298082636c2f25bfc3f6f5c9afef
Josh Brunner <josh@helpfulhuman.com>
2017-02-05 08:15:37 -0800

    Set up base cube class and styles

---
+++ src/Components/Shared/Cube.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../Styles/Cube.css';
+
+export default class Cube extends React.Component {
+  render () {
+    return (
+      <div className='cube'>
+        <div className='cube__container'>
+          Cube!
+        </div>
+      </div>
+    )
+  }
+}
