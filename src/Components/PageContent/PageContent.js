9433c0307c477e2a4f9c0bf5d13a8b83663364fa
Josh Brunner <josh@helpfulhuman.com>
2017-02-19 15:31:30 -0800

    Made the PageContent component a little more dynamic in it's handling

--- src/Components/PageContent/PageContent.js
+++ src/Components/PageContent/PageContent.js
@@ -5,22 +5,34 @@ import '../../Styles/Grid.css';
 
 export default class PageContent extends React.Component {
 
+  renderLeftSide () {
+    const { leftSide } = this.props;
+    return leftSide ? (
+      <div className={leftSide.classes}>
+        {leftSide.content}
+      </div>
+    ) : null
+  }
+
+  renderRightSide () {
+    const { rightSide } = this.props;
+    return rightSide ? (
+      <div className={rightSide.classes}>
+        {rightSide.content}
+      </div>
+    ) : null
+  }
+
   render () {
-
-    const { fontSize, leftSide, rightSide } = this.props;
-
+    const { fontSize } = this.props;
     return (
       <div
         className='page-content'
         style={{ fontSize: (fontSize / Constants.fontSize.base) + 'em' }}>
         <div className='page-content__container'>
           <div className='row'>
-              <div className={leftSide.classes}>
-                  {leftSide.content}
-              </div>
-              <div className={rightSide.classes}>
-                  {rightSide.content}
-              </div>
+            {this.renderLeftSide()}
+            {this.renderRightSide()}
           </div>
         </div>
       </div>
