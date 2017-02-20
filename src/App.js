970ca3b4d224b281d2c31ca72cfde96933d5919d
Josh Brunner <josh@helpfulhuman.com>
2017-02-20 06:07:43 -0800

    First attempt at animating pages in via opacity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import className from 'classnames';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import Cube from './Components/Shared/Cube';
 import Constants from './Constants';
 import Theme from './Components/Theme';
@@ -46,16 +47,54 @@ export default class App extends React.Component {
             height={this.state.headerHeight}
             isAtRoot={isAtRoot}
             />
-          <div
+          <ReactCSSTransitionGroup
+            component='div'
             className='app__content'
+            transitionName='example'
+            transitionEnterTimeout={1000}
+            transitionLeaveTimeout={1000}
+            >
+            <div
+            key={this.props.location.pathname}
             style={{
-              marginTop: this.state.headerHeight,
-              maxWidth: Constants.screenWidth
+              position: 'fixed',
+                left: '50%',
+                height: '100%',
+                width: '100%',
+                transform: 'translate(-50%, 0)'
             }}>
-            {this.props.children}
-          </div>
+            <Background
+
+              styles={{
+                // top: 0,
+                // left: 0,
+                // position: 'fixed',
+                // width: '100%',
+                // height: '100%',
+                // position: 'fixed',
+                // left: '50%',
+                // transform: 'translate(-50%, 0)',
+                margin: '0 auto',
+                height: '100%',
+                display: 'flex',
+                justifyContent: 'center',
+                overflowY: 'auto',
+                WebkitOverflowScrolling: 'touch',
+                paddingTop: this.state.headerHeight,
+                maxWidth: Constants.screenWidth
+              }}>
+              {this.props.children}
+            </Background>
+            </div>
+          </ReactCSSTransitionGroup>
         </div>
       </div>
     );
+  }
+}
+
+class Background extends React.Component {
+  render () {
+    return (<div style={this.props.styles}>{this.props.children}</div>)
   }
 }
--- src/Components/Pages/Education.js
+++ src/Components/Pages/Education.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router';
+
 import PageContent from '../PageContent/PageContent';
 
 const paragraph1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
@@ -11,6 +13,7 @@ export default class Education extends React.Component {
       <div className='page-content__section'>
         <div className='page-content__section-header'>
           Education
+          <Link to='/personal' >Link</Link>
         </div>
         <div className='page-content__section-body'>
           {paragraph1}
