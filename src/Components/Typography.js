3cb63216d446d67f5297541ecdbecdbb729c6030
Josh Brunner <josh@helpfulhuman.com>
2017-02-22 22:26:05 -0800

    Added a link to the typography styles

--- src/Components/Typography.js
+++ src/Components/Typography.js
@@ -18,11 +18,12 @@ export default class Typography extends React.Component {
               <li><p><strong>Bolded body content</strong></p></li>
               <li><p><i>Italic body content</i></p></li>
               <li><p>Regular body content</p></li>
+              <li><a href='#'>Link</a></li>
             </ul>
           </div>
           <div className='col-xs-12 col-sm-6'>
             <h3>Example</h3>
-            <p>Lorem <b>ipsum dolor sit amet</b>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut <i>labore et dolore magna aliqua. Ut enim</i> ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. <i>Excepteur sint occaecat</i> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem <b>ipsum dolor sit amet</b>, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut <i>labore et dolore magna aliqua. Ut enim</i> ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse <a href='#'>cillum dolore eu</a> fugiat nulla pariatur. <i>Excepteur sint occaecat</i> cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore <b>eu fugiat nulla</b> pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod <i>tempor incididunt ut labore et </i>dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in <b>culpa qui officia deserunt mollit</b> anim id est laborum.</p>
           </div>
