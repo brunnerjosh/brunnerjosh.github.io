5f3647352c2472927ddc968b7db7605c2821062a
Josh Brunner <josh@helpfulhuman.com>
2017-02-28 22:01:31 -0800

    Remove dependency on deprecated number incrementer code

--- src/Containers/Landing.js
+++ src/Containers/Landing.js
@@ -1,18 +1,13 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import Landing from '../Components/Landing';
-import { addNumber, subtractNumber } from '../Actions/Number.js';
 
 function mapStateToProps (state) {
-  return {
-    number: state.number.value
-  };
+  return {};
 }
 
 function mapDispatchToProps (dispatch) {
   return bindActionCreators({
-    addNumber,
-    subtractNumber
   }, dispatch);
 }
 
