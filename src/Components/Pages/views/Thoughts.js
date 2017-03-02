6f75dd6b4400647d71b7ffcc863d35eb633f48a8
Josh Brunner <josh@helpfulhuman.com>
2017-03-01 22:47:54 -0800

    Lots of work on styling and displaying Medium posts on the Thoughts page

--- src/Components/Pages/views/Thoughts.js
+++ src/Components/Pages/views/Thoughts.js
@@ -1,38 +1,95 @@
+/*
+See this link for potential to load more than 10 items in RSS feed
+https://help.medium.com/hc/en-us/articles/225170228-Setting-full-and-partial-RSS-feeds-for-publications-and-profiles
+*/
 import React from 'react';
+import moment from 'moment';
+import Constants from '../../../Constants';
 import PageContent from '../PageContainer';
 import '../../../Styles/Medium.css';
 
-const MEDIUM_USERNAME = 'dourvaris'
+const MEDIUM_DATE_FORMAT = 'ddd, D MMM YYYY HH:mm:ss Z';
 
 export default class Thoughts extends React.Component {
 
   componentWillMount () {
     // Makes sure not to reload the Medium posts if we already loaded them
     if (this.props.medium && !this.props.medium.stories) {
-      this.props.fetchMediumFeed(MEDIUM_USERNAME)
+      this.props.fetchMediumFeed(Constants.handle)
     }
   }
 
-  renderMediumArticle (articles) {
-    // Only return if the post has categories (meaning it's an actual post)
-    return articles.map( (article, index) => {
-      return article.category ? (
+  renderMediumArticles (articles) {
+
+    const chunkededByMonth = [];
+
+    /**
+     * Loops through the given articles and adds them to a sorted array by publish date
+     */
+    articles.forEach( (article, index) => {
+
+      // Prevent adding "Medium comments" to final array
+      if (!article.category) return;
+
+      const articleDate = moment(article.pubdate, MEDIUM_DATE_FORMAT)
+                          .format('MMMM, YYYY');
+
+      const currentArticle = (
         <div key={index}>
-          <div>{article.title}</div>
-          <div>Created: {article.created}</div>
-          <div dangerouslySetInnerHTML={{ __html: article.description }} />
+          <div className='medium-feed__container'>
+            <h2
+              className={'medium-feed-title'}
+              onClick={() => window.open(article.link, '_blank')}>
+              {article.title}
+            </h2>
+            <div dangerouslySetInnerHTML={{ __html: article.description }} />
+          </div>
         </div>
-      ) : null;
-    })
+      )
+
+      // See if we can find an existing array to push this next article to
+      for (let month of chunkededByMonth) {
+        if (month.date === articleDate) {
+          month.items.push(currentArticle);
+          return;
+        }
+      }
+
+      // Didn't find an array, create a new entry and add it there
+      chunkededByMonth.push({
+        date: articleDate,
+        items: [currentArticle]
+      });
+
+    });
+
+    // Now that we have sorted the articles, render them with the date spacer
+    return chunkededByMonth.length ? chunkededByMonth.map( (month, index) => {
+      return (
+        <div key={index}>
+          <div className='medium-feed-spacer'>{month.date}</div>
+          {month.items}
+        </div>
+      )
+    }) : (
+      <div>
+      No posts yet :(
+      </div>
+    )
   }
 
   renderPageContent () {
+
+    const content = this.props.medium.stories ? this.renderMediumArticles(this.props.medium.stories.items) : (
+      <h2>Loading...</h2>
+    );
+
     return (
       <div>
         <h1>Thoughts</h1>
         <p>I use Medium to write down the majority of my thoughts. Here are my most recent posts:</p>
-        <button onClick={this.props.fetchMediumFeed.bind(null, MEDIUM_USERNAME)}>Reload posts</button>
-        {this.props.medium.stories && this.renderMediumArticle(this.props.medium.stories.items)}
+        <button onClick={this.props.fetchMediumFeed.bind(null, Constants.handle)}>Reload posts</button>
+        {content}
       </div>
     )
   }
