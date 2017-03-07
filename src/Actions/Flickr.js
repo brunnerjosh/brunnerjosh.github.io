fa2d0dcc0c8450af9e4cbd7dc96f48a77757aa36
Josh Brunner <josh@helpfulhuman.com>
2017-03-06 22:02:54 -0800

    Added Flickr integration to the Redux store

---
+++ src/Actions/Flickr.js
@@ -0,0 +1,18 @@
+import Flickr from 'node-flickr';
+
+const flickr = new Flickr({
+  api_key: process.env.REACT_APP_FLICKR_KEY
+});
+
+export function fetchFlickrPhotos (opts) {
+  return dispatch => {
+    dispatch({ type: 'FETCHING_FLICKR' });
+    flickr.get('people.getPublicPhotos', opts, (err, result) => {
+      if (err) {
+        dispatch({ type: 'FETCH_FLICKR_ERROR', err });
+      } else {
+        dispatch({ type: 'FETCH_FLICKR_SUCCESS', photos: result.photos });
+      }
+    })
+  }
+}
