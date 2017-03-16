b4a613710cd556ccaa7816a905d0b91b1b8aa7f9
Josh Brunner <josh@helpfulhuman.com>
2017-03-15 22:41:28 -0700

    Brought the Card component over to the PErsonal page

--- src/Components/Pages/views/Personal.js
+++ src/Components/Pages/views/Personal.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router';
 import PageContent from '../PageContainer';
+import Card from '../../Shared/Card';
 
 export default class Personal extends React.Component {
 
@@ -10,10 +10,21 @@ export default class Personal extends React.Component {
         <h1>Personal Projects</h1>
         <p>When I'm not hiking in the mountains, I'm probably behind a computer screen learning new technologies or practicing something I may need to learn for work. Here are a few of the projects that have actually seen the light of day.</p>
         <div className='content__section-spacer'>April, 2015</div>
-        <Link to='/articles/js_slideshow'><h2>Vanilla JavaScript Slideshow</h2></Link>
+        <Card
+          header={'JavaScript Slideshow'}
+          onClick={this.props.router.push.bind(null, '/articles/js_slideshow')}
+          description={'After reading an article written by Chris Zacharias, a former YouTube employee, I was inspired to begin practicing various JavaScript problems. The goal I have since set for myself is...'}/>
         <div className='content__section-spacer'>Coming soon</div>
-        <h2 className='+disabled'>Video Chat</h2>
-        <h2 className='+disabled'>Dashboard</h2>
+        <Card
+          header={'Video Chat'}
+          onClick={null}
+          disabled={true}
+          description={'Coming soon...'}/>
+        <Card
+          header={'Dashboard'}
+          onClick={null}
+          disabled={true}
+          description={'Coming soon...'}/>
       </div>
     )
   }
