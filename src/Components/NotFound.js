f3e2440afca293f24417aecac40bb87e1faf983d
Josh Brunner <joshuaebrunner@gmail.com>
2017-03-26 09:53:29 -0700

    Added an auto redirect for the NotFound component

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -2,6 +2,37 @@ import React from 'react';
 import { Link } from 'react-router';
 
 export default class NotFound extends React.Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      timeout: props.timeout
+    };
+    this.timer = this.timer.bind(this);
+  }
+
+  componentDidMount () {
+    this.setState({
+      intervalId: setInterval(this.timer, 1000)
+    });
+  }
+
+  componentWillUnmount () {
+    clearInterval(this.state.intervalId);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.timeout === 0) {
+      this.props.router.push('/');
+      return false;
+    }
+    return true;
+  }
+
+  timer () {
+    this.setState({ timeout: this.state.timeout - 1 });
+  }
+
   render () {
     return (
       <div style={{textAlign: 'center'}}>
@@ -9,8 +40,13 @@ export default class NotFound extends React.Component {
         <h2>PAGE NOT FOUND</h2>
         <hr />
         <p>Sorry, but the page you are looking for does not exist.</p>
+        <p><em>Redirecting in {this.state.timeout}</em></p>
         <Link to='/'>Take me home</Link>
       </div>
     )
   }
+}
+
+NotFound.defaultProps = {
+  timeout: 5
 }
