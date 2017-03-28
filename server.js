9aacb7af29371f723e285375d5f3de422d797e9a
Josh Brunner <josh@helpfulhuman.com>
2017-03-27 18:06:59 -0700

    Got rid of the need for a static.json file

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 require('dotenv').config();
-const express = require('express');
 const path = require('path');
+const express = require('express');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 8080;
-const app = express();
-const config = require('./static.json');
 const routes = require('./routes/api-router');
 
-app.use(bodyParser.json()); // for parsing application/json
+const app = express();
+const port = process.env.PORT || 8080;
+const config = {
+  root: 'build/',
+  home: 'index.html'
+};
+
+app.use(bodyParser.json());                         // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use('/api', routes);
 app.use(express.static(__dirname + `/${config.root}`));
 
-app.get('*', (req, res) => res.sendFile( path.resolve(__dirname, config.root, config.routes["/**"])) );
+app.get('*', (req, res) => res.sendFile( path.resolve(__dirname, config.root, config.home)) );
 
 app.listen(port, function (err) {
   if (err) {
