4e202861174be5632842b6ca821ccb6a54bea11c
Josh Brunner <josh@helpfulhuman.com>
2017-05-03 21:58:37 -0700

    Added screenFull and screenNormal svg icons

--- src/Components/Icon/Icons.js
+++ src/Components/Icon/Icons.js
@@ -448,7 +448,25 @@ export function Arduino () {
   )
 }
 
+export function screenFull () {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 16">
+      <path fillRule="evenodd" d="M13 10h1v3c0 .546875-.453125 1-1 1h-3v-1h3v-3zM1 10H0v3c0 .546875.453125 1 1 1h3v-1H1v-3zm0-7h3V2H1c-.546875 0-1 .453125-1 1v3h1V3zm1 1h10v8H2V4zm2 6h6V6H4v4zm6-8v1h3v3h1V3c0-.546875-.453125-1-1-1h-3z"/>
+    </svg>
+  )
+}
+
+export function screenNormal () {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 14 16">
+      <path fillRule="evenodd" d="M2 4H0V3h2V1h1v2c0 .546875-.453125 1-1 1zm0 8H0v1h2v2h1v-2c0-.546875-.453125-1-1-1zm9-2c0 .546875-.453125 1-1 1H4c-.546875 0-1-.453125-1-1V6c0-.546875.453125-1 1-1h6c.546875 0 1 .453125 1 1v4zM9 7H5v2h4V7zm2 6v2h1v-2h2v-1h-2c-.546875 0-1 .453125-1 1zm1-10V1h-1v2c0 .546875.453125 1 1 1h2V3h-2z"/>
+    </svg>
+  )
+}
+
 export default {
+  screenFull,
+  screenNormal,
   W,
   UWBSTEM,
   FilePDF,
