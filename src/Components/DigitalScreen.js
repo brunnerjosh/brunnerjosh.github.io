08e804f81f9fb7dd12ce9fedc20ee05929bd37aa
Josh Brunner <joshuaebrunner@gmail.com>
2018-06-25 22:50:19 -0700

    Got the initial grid to display

--- src/Components/DigitalScreen.js
+++ src/Components/DigitalScreen.js
@@ -7,15 +7,38 @@ export default class Screen extends React.Component {
     super(props);
 
     this.state = {
-
+      screen: [
+        ['', '', '', ''],
+        ['', '', '', ''],
+        ['', '', '', ''],
+        ['', '', '', '']
+      ]
     };
 
+    this.renderRow = this.renderRow.bind(this)
+    this.renderPixel = this.renderPixel.bind(this)
+  }
+
+  renderPixel (data) {
+    return (
+      <div className='screen__row-pixel'>
+      {data || '__'}
+      </div>
+    )
+  }
+
+  renderRow (row) {
+    return (
+      <div className='screen__row'>
+      {row.map(this.renderPixel)}
+      </div>
+    )
   }
 
   render () {
     return (
       <div className={'screen'}>
-        Digital Screen
+        {this.state.screen.map(this.renderRow)}
       </div>
     )
   }
